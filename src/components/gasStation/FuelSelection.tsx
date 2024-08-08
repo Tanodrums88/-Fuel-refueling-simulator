64f5f91a49e8694bb06f32a8b8fdbfc554bb9136
Tanodrums <gaetano.gelo@live.com>
2024-08-08 18:23:42 +0200

    Migliorati stile in modalità mobile

--- src/components/gasStation/FuelSelection.tsx
+++ src/components/gasStation/FuelSelection.tsx
@@ -45,7 +45,7 @@ export default function FuelSelection() {
   }, [FuelName, FuelPrice, FuelTank, FuelPresent, MissingFuel]);
 
   return (
-    <FormControl className="fuelSelection">
+    <FormControl className="fuelSelection" style={{margin:'auto 10vw'}}>
       <FormLabel id="demo-row-radio-buttons-group-label">
         Select the fuel type
       </FormLabel>
