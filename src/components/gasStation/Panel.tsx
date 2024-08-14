6cc90250696e08e7dd6747cc0582d4a2217d43e5
Tanodrums <gaetano.gelo@live.com>
2024-08-14 02:41:19 +0200

    modificato intervallo contatore

--- src/components/gasStation/Panel.tsx
+++ src/components/gasStation/Panel.tsx
@@ -2,70 +2,70 @@ import React, { useEffect, useState } from "react";
 import { useRefuelingContext } from "../../store/RefuelingContext";
 
 function Panel() {
-  const { refuelings, refuelingInProgress, selectorIsActive } =
-    useRefuelingContext();
+	const { refuelings, refuelingInProgress, selectorIsActive } =
+		useRefuelingContext();
 
-  const [l, setL] = useState<number>(0);
-  const [intervalId, setIntervalId] = useState<number>(0);
+	const [l, setL] = useState<number>(0);
+	const [intervalId, setIntervalId] = useState<number>(0);
 
-  refuelings.fuelInserted = l;
+	refuelings.fuelInserted = l;
 
-  const stopRefuelingIsFull = () => {
-    clearInterval(intervalId);
-  };
+	const stopRefuelingIsFull = () => {
+		clearInterval(intervalId);
+	};
 
-  useEffect(() => {
-    if (refuelingInProgress) {
-      let newIntervalId = setInterval(() => {
-        setL((l) => l + 0.054);
-      }, 50);
-      setIntervalId(newIntervalId);
-    }
-    if (!refuelingInProgress) {
-      return stopRefuelingIsFull();
-    }
-  }, [refuelingInProgress]);
+	useEffect(() => {
+		if (refuelingInProgress) {
+			let newIntervalId = setInterval(() => {
+				setL((l) => l + 0.01);
+			}, 10);
+			setIntervalId(newIntervalId);
+		}
+		if (!refuelingInProgress) {
+			return stopRefuelingIsFull();
+		}
+	}, [refuelingInProgress]);
 
-  const formattedLitres = l.toFixed(2);
-  const price = refuelings.price * l;
-  const formattedPrice = price.toFixed(2);
+	const formattedLitres = l.toFixed(2);
+	const price = refuelings.price * l;
+	const formattedPrice = price.toFixed(2);
 
-  refuelings.fuelInserted = +formattedLitres;
-  refuelings.amount = +formattedPrice;
+	refuelings.fuelInserted = +formattedLitres;
+	refuelings.amount = +formattedPrice;
 
-  let panelClass: string = refuelingInProgress
-    ? "panelValue panelIsActive"
-    : "panelValue panelIsNotActive";
+	let panelClass: string = refuelingInProgress
+		? "panelValue panelIsActive"
+		: "panelValue panelIsNotActive";
 
-  let priceContent: any = selectorIsActive.amountSelected;
+	let priceContent: any = selectorIsActive.amountSelected;
 
-  if (refuelingInProgress) {
-    priceContent = formattedPrice;
-  }
-  if (!refuelingInProgress && l > 0) {
-    priceContent = formattedPrice;
-  }
+	if (refuelingInProgress) {
+		priceContent = formattedPrice;
+	}
+	if (!refuelingInProgress && l > 0) {
+		priceContent = formattedPrice;
+	}
 
-  return (
-    <div className="panel">
-      <div className="boxPanel1">
-        <div className={panelClass}>
-          <ul>
-            <li>{priceContent}</li>
-            <li>{formattedLitres}</li>
-            <li>{refuelings.price}</li>
-          </ul>
-        </div>
-        <div className="panelLegend">
-          <ul>
-            <li>€</li>
-            <li>L</li>
-            <li>€/L</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='panel'>
+			<div className='boxPanel1'>
+				<div className={panelClass}>
+					<ul>
+						<li>{priceContent}</li>
+						<li>{formattedLitres}</li>
+						<li>{refuelings.price}</li>
+					</ul>
+				</div>
+				<div className='panelLegend'>
+					<ul>
+						<li>€</li>
+						<li>L</li>
+						<li>€/L</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Panel;
--- src/components/gasStation/ProgressBar.tsx
+++ src/components/gasStation/ProgressBar.tsx
@@ -2,51 +2,58 @@ import React, { useEffect, useState } from "react";
 import { useRefuelingContext } from "../../store/RefuelingContext";
 
 function ProgressBar() {
-  const { refuelingInProgress, refuelings, stopRefueling, selectorIsActive } =
-    useRefuelingContext();
+	const { refuelingInProgress, refuelings, stopRefueling, selectorIsActive } =
+		useRefuelingContext();
 
-  const [l, setL] = useState<number>(0);
-  const [intervalId, setIntervalId] = useState<number>(0);
+	const [l, setL] = useState<number>(0);
+	const [intervalId, setIntervalId] = useState<number>(0);
 
-  let missingFuel = refuelings.missingFuel;
+	let missingFuel = refuelings.missingFuel;
 
-  if (l >= missingFuel) {
-    refuelings.refuelingComplete = true;
-  }
+	if (l >= missingFuel) {
+		refuelings.refuelingComplete = true;
+	}
 
-  let amountComplete = selectorIsActive.amountSelected;
+	let amountComplete = selectorIsActive.amountSelected;
 
-  if (refuelings.amount >= amountComplete && refuelingInProgress && selectorIsActive.active) {
-    refuelings.refuelingComplete = true;
-  }
+	if (
+		refuelings.amount >= amountComplete &&
+		refuelingInProgress &&
+		selectorIsActive.active
+	) {
+		refuelings.refuelingComplete = true;
+	}
 
-  const stopRefuelingIsFull = () => {
-    clearInterval(intervalId);
-  };
+	const stopRefuelingIsFull = () => {
+		clearInterval(intervalId);
+	};
 
-  useEffect(() => {
-    if (refuelingInProgress) {
-      let newIntervalId = setInterval(() => {
-        setL((l) => l + 0.054);
-      }, 50);
-      setIntervalId(newIntervalId);
-    }
-    if (!refuelingInProgress) {
-      stopRefuelingIsFull();
-    }
-    if (refuelings.refuelingComplete) {
-      stopRefueling();
-      stopRefuelingIsFull();
-    }
-  }, [refuelingInProgress, refuelings.refuelingComplete]);
+	useEffect(() => {
+		if (refuelingInProgress) {
+			let newIntervalId = setInterval(() => {
+				setL((l) => l + 0.01);
+			}, 10);
+			setIntervalId(newIntervalId);
+		}
+		if (!refuelingInProgress) {
+			stopRefuelingIsFull();
+		}
+		if (refuelings.refuelingComplete) {
+			stopRefueling();
+			stopRefuelingIsFull();
+		}
+	}, [refuelingInProgress, refuelings.refuelingComplete]);
 
-  return (
-    <>
-      <div className="containerProgressBar">
-        <progress max={refuelings.missingFuel} value={l} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='containerProgressBar'>
+				<progress
+					max={refuelings.missingFuel}
+					value={l}
+				/>
+			</div>
+		</>
+	);
 }
 
 export default ProgressBar;
