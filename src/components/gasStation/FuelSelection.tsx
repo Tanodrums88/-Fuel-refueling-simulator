6cbae90a26a8217109b5cd040b5659114669302a
Tanodrums <gaetano.gelo@live.com>
2024-09-12 22:00:18 +0200

    Spostate le funzioni random per la capienza dei serbatoi dentro il file FuelSelection all'interno di useEffect in modo da richiamarle ad ogni cambiamento di selezione

--- src/components/gasStation/FuelSelection.tsx
+++ src/components/gasStation/FuelSelection.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {
-  FormLabel,
-  FormControl,
-  FormControlLabel,
-  RadioGroup,
-  Radio,
+	FormLabel,
+	FormControl,
+	FormControlLabel,
+	RadioGroup,
+	Radio,
 } from "@mui/material";
 
 import { Fuel } from "../../data/FuelData";
@@ -12,74 +12,80 @@ import { Fuel } from "../../data/FuelData";
 import { useRefuelingContext } from "../../store/RefuelingContext";
 
 export default function FuelSelection() {
-  const { refuelingCount, refuelings, refuelingInProgress } =
-    useRefuelingContext();
+	const { refuelingCount, refuelings, refuelingInProgress } =
+		useRefuelingContext();
 
-  const [value, setValue] = useState(refuelings.name);
+	const [value, setValue] = useState(refuelings.name);
 
-  function selectFuel(event: React.ChangeEvent<HTMLInputElement>) {
-    setValue((event.target as HTMLInputElement).value);
-  }
+	function selectFuel(event: React.ChangeEvent<HTMLInputElement>) {
+		setValue((event.target as HTMLInputElement).value);
+	}
 
-  const FuelFilter = Fuel.filter((el) => {
-    return el.name === value;
-  });
+	const FuelFilter = Fuel.filter((el) => {
+		return el.name === value;
+	});
 
-  const FuelName: string = FuelFilter[0].name;
-  const FuelPrice: number = FuelFilter[0].price;
-  const FuelTank: number = FuelFilter[0].fuelTank;
-  const FuelPresent: number = FuelFilter[0].fuelPresent;
-  const MissingFuel: number = FuelFilter[0].missingFuel;
+	const FuelName: string = FuelFilter[0].name;
+	const FuelPrice: number = FuelFilter[0].price;
 
-  useEffect(() => {
-    refuelingCount({
-      name: FuelName,
-      price: FuelPrice,
-      fuelTank: FuelTank,
-      fuelPresent: FuelPresent,
-      missingFuel: MissingFuel,
-      fuelInserted: 0,
-      refuelingComplete: false,
-      amount: 0,
-    });
-  }, [FuelName, FuelPrice, FuelTank, FuelPresent, MissingFuel]);
+	useEffect(() => {
+		const fuelTank = Math.floor(Math.random() * (91 - 75)) + 75;
+		const fuelPresent = Math.floor(Math.random() * (fuelTank - 5)) + 5;
+		const missingFuel = fuelTank - fuelPresent;
 
-  return (
-    <FormControl className="fuelSelection" style={{margin:'auto 10vw'}}>
-      <FormLabel id="demo-row-radio-buttons-group-label">
-        Select the fuel type
-      </FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-        value={value}
-        onChange={selectFuel}>
-        <FormControlLabel
-          disabled={refuelingInProgress ? true : false}
-          value={"PETROL"}
-          control={<Radio />}
-          label={"PETROL"}
-        />
-        <FormControlLabel
-          disabled={refuelingInProgress ? true : false}
-          value={"DIESEL"}
-          control={<Radio />}
-          label={"DIESEL"}
-        />
-        <FormControlLabel
-          disabled={refuelingInProgress ? true : false}
-          value={"LPG"}
-          control={<Radio />}
-          label={"LPG"}
-        />
-        <FormControlLabel
-          disabled={refuelingInProgress ? true : false}
-          value={"METHANE"}
-          control={<Radio />}
-          label={"METHANE"}
-        />
-      </RadioGroup>
-    </FormControl>
-  );
+		refuelingCount({
+			name: FuelName,
+			price: FuelPrice,
+			fuelTank: fuelTank,
+			fuelPresent: fuelPresent,
+			missingFuel: missingFuel,
+			fuelInserted: 0,
+			refuelingComplete: false,
+			amount: 0,
+		});
+		console.log(
+			`Capienza serbatoio =${fuelTank}, Carburante presente =${fuelPresent}, Carburante mancante =${missingFuel}`
+		);
+	}, [FuelName, FuelPrice]);
+
+	return (
+		<FormControl
+			className='fuelSelection'
+			style={{ margin: "auto 10vw" }}>
+			<FormLabel id='demo-row-radio-buttons-group-label'>
+				Select the fuel type
+			</FormLabel>
+			<RadioGroup
+				row
+				aria-labelledby='demo-row-radio-buttons-group-label'
+				name='row-radio-buttons-group'
+				value={value}
+				onChange={selectFuel}>
+				<FormControlLabel
+					disabled={refuelingInProgress ? true : false}
+					value={"PETROL"}
+					control={<Radio />}
+					label={"PETROL"}
+				/>
+				<FormControlLabel
+					disabled={refuelingInProgress ? true : false}
+					value={"DIESEL"}
+					control={<Radio />}
+					label={"DIESEL"}
+				/>
+				<FormControlLabel
+					disabled={refuelingInProgress ? true : false}
+					value={"LPG"}
+					control={<Radio />}
+					label={"LPG"}
+				/>
+				<FormControlLabel
+					disabled={refuelingInProgress ? true : false}
+					value={"METHANE"}
+					control={<Radio />}
+					label={"METHANE"}
+				/>
+			</RadioGroup>
+		</FormControl>
+	);
 }
--- src/data/FuelData.tsx
+++ src/data/FuelData.tsx
@@ -1,48 +1,44 @@
 import { type FuelSelection } from "../store/RefuelingContext";
 
-const fuelTank = Math.floor(Math.random() * (91 - 75)) + 75;
-const fuelPresent = Math.floor(Math.random() * (fuelTank - 5)) + 5;
-const missingFuel = fuelTank - fuelPresent;
-
 export const Fuel: Array<FuelSelection> = [
-  {
-    name: "PETROL",
-    price: 1.897,
-    fuelTank: fuelTank,
-    fuelPresent: fuelPresent,
-    missingFuel: missingFuel,
-    fuelInserted: 0,
-    refuelingComplete: false,
-    amount: 0,
-  },
-  {
-    name: "DIESEL",
-    price: 1.809,
-    fuelTank: fuelTank,
-    fuelPresent: fuelPresent,
-    missingFuel: missingFuel,
-    fuelInserted: 0,
-    refuelingComplete: false,
-    amount: 0,
-  },
-  {
-    name: "LPG",
-    price: 0.698,
-    fuelTank: fuelTank,
-    fuelPresent: fuelPresent,
-    missingFuel: missingFuel,
-    fuelInserted: 0,
-    refuelingComplete: false,
-    amount: 0,
-  },
-  {
-    name: "METHANE",
-    price: 1.304,
-    fuelTank: fuelTank,
-    fuelPresent: fuelPresent,
-    missingFuel: missingFuel,
-    fuelInserted: 0,
-    refuelingComplete: false,
-    amount: 0,
-  },
+	{
+		name: "PETROL",
+		price: 1.897,
+		fuelTank: 0,
+		fuelPresent: 0,
+		missingFuel: 0,
+		fuelInserted: 0,
+		refuelingComplete: false,
+		amount: 0,
+	},
+	{
+		name: "DIESEL",
+		price: 1.809,
+		fuelTank: 0,
+		fuelPresent: 0,
+		missingFuel: 0,
+		fuelInserted: 0,
+		refuelingComplete: false,
+		amount: 0,
+	},
+	{
+		name: "LPG",
+		price: 0.698,
+		fuelTank: 0,
+		fuelPresent: 0,
+		missingFuel: 0,
+		fuelInserted: 0,
+		refuelingComplete: false,
+		amount: 0,
+	},
+	{
+		name: "METHANE",
+		price: 1.304,
+		fuelTank: 0,
+		fuelPresent: 0,
+		missingFuel: 0,
+		fuelInserted: 0,
+		refuelingComplete: false,
+		amount: 0,
+	},
 ];
